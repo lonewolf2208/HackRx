4d6660b808b2bc6c06c0ef30f7c9a90cee227f71
Kunal <kunalmehrotra2001@gmail.com>
2023-07-21 10:39:36 +0530

    Created Bottiom navigation framework

--- app/src/main/java/com/example/hackrx40/view/MainActivity.kt
+++ app/src/main/java/com/example/hackrx40/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.hackrx40
+package com.example.hackrx40.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hackrx40.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
